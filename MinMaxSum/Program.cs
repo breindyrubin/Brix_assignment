0045a3afc4a38042d0aa3ef6d14a9fb06dc46c75
breindyrubin <86554362+breindyrubin@users.noreply.github.com>
2021-11-23 12:43:10 +0200

    Add files via upload

---
+++ MinMaxSum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxSum
+{
+    class Program
+    {
+        public static void PrintMinMaxSum( long [] arrNumbers)
+        {
+            long minSum = 0;
+            long maxSum = 0;
+            Array.Sort(arrNumbers);
+
+            for (int i = 0; i < 4; i++)
+            {
+                minSum += arrNumbers[i];
+            }
+
+            for (int i = 4; i > 0; i--)
+            {
+                maxSum += arrNumbers[i];
+            }
+            Console.WriteLine($"min sum:{minSum} max sum:{maxSum}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter five numbers with spaces in between:");
+            string numbers = Console.ReadLine();
+            long[] numbersArr = numbers.Split(' ').Select(Int64.Parse).ToArray();
+            if (numbersArr.Length > 5 || numbersArr.Length < 6)
+            {
+                throw new FormatException( "input too long/short!");
+            }
+            else
+            {
+                PrintMinMaxSum(numbersArr);
+                Console.ReadLine();
+            }
+        }
+    }
+}
