bdfa7dc3e85fe419edfaea97600c0edb3d0070ff
breindyrubin <86554362+breindyrubin@users.noreply.github.com>
2021-11-23 04:49:02 +0200

    Add files via upload

---
+++ Recursive/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive
+{
+    class Program
+    {
+        public static int SuperDigit(string n,int k=-1)
+        {
+           
+            string p=n;
+            int superDigit=0;
+            int stringToInt;
+            if (k != -1) { 
+            for (int i = 1; i < k; i++)
+                p += n;
+            }
+            
+                for (int i = 0; i < p.Length; i++)
+                {
+                    stringToInt = Int32.Parse(p[i].ToString());
+                    superDigit += stringToInt;
+                }
+            if (superDigit/10==0)
+                return superDigit;
+            return SuperDigit(superDigit.ToString());
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number :");
+           string a = Console.ReadLine();
+            Console.WriteLine("please enter a number :");
+           string b = Console.ReadLine();
+            int g = SuperDigit(a, Int32.Parse(b));
+            Console.WriteLine(g);
+            Console.Read();
+        }
+    }
+}
